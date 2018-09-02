038dbba61043fcd04b5385ec1d1138db15146c74
Mire Koda <sai7aku8@gmail.com>
2018-09-03 03:44:21 +0900

    蟻本が読めない

---
+++ No299/Program.cs
@@ -0,0 +1,8 @@
+using System;
+class Program{
+    static void Main(string[]args){
+        long N=long.Parse(Console.ReadLine());
+        long page=N-1;
+        Console.WriteLine(316+(52*page));
+    }
+}
